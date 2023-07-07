6939ef42490a94c1524ce7a476f4fb4f65783530
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2023-07-07 19:56:40 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "vijay-terraform-elb"
-  availability_zones = ["ap-south-1a", "ap-south-1c"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
